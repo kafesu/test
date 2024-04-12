2218a3ef6db48b6ff4115a3ce1220d151d66ce0c
kafesu <anesukafesu@gmail.com>
2024-04-12 10:10:53 +0200

    Update login-company.php

--- login-company.php
+++ login-company.php
@@ -12,7 +12,7 @@ if(isset($_SESSION['id_user']) || isset($_SESSION['id_company'])) {
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>Takura Job Place</title>
+  <title>Lynk</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
@@ -39,7 +39,7 @@ if(isset($_SESSION['id_user']) || isset($_SESSION['id_company'])) {
 <body class="hold-transition login-page">
 <div class="login-box">
   <div class="login-logo">
-    <a href="index.php"><b>Job</b> Portal</a>
+    <a href="index.php">Lynk</a>
   </div>
   <!-- /.login-logo -->
   <div class="login-box-body">
